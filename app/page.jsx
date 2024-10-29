1c2915b289c477b952f15805dadbb2462f0a5b01
jaks17 <151195241+jaks17@users.noreply.github.com>
2024-10-29 12:20:38 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
-//components
+// Components
 import Social from "@/components/Social";
 import Photo from "@/components/Photo";
 import Stats from "@/components/Stats";
@@ -11,28 +11,30 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-          {/* text */}
+          {/* Text */}
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Developer</span>
             <h1 className="h1 mb-6">
-              Hello I'm <br/><span className="text-accent">Gaddapu Jagan Kumar</span>
+              Hello I'm <br /><span className="text-accent">Gaddapu Jagan Kumar</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-            Creating Elegant and User-Friendly Digital Experiences | Skilled in Full-Stack Development, Microservices, and Modern Tools.
+              Creating Elegant and User-Friendly Digital Experiences | Skilled in Full-Stack Development, Microservices, and Modern Tools.
             </p>
-            {/* bto and socials */}
+            {/* Button and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" size="lg" className="uppercase flex item-center gap-2">
-                <span>Download CV</span>
-                <FiDownload className="text-xl"/>
-              </Button>
+              <a href="/assets/Resume/jagtkm.pdf" download>
+                <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
-                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+                <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
               </div>
             </div>
-            </div>
-          {/* photo */}
-          <div className="order-1 xl:orden-none mb-8 xl:mb-0">
+          </div>
+          {/* Photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
